385aaae793bde66c2240736a875b821227492caf
nebula6684 <nebula6684@gmail.com>
2023-01-10 10:25:00 +0900

    light2d

--- Assets/Script/Manager/StageManager.cs
+++ Assets/Script/Manager/StageManager.cs
@@ -6,17 +6,18 @@ public class StageManager : MonoBehaviour
 {
     [SerializeField] private GameObject grid;
     [SerializeField] private GameObject keyPos;
-    [SerializeField] private GameObject objects;
     [SerializeField] private GameObject key;
+    [SerializeField] private GameObject playerPos;
+    [SerializeField] private GameObject player;
 
     void Start()
     {
         Json.Instance.Read();
 
         grid.transform.GetChild(Json.Instance.data.nowGameStage - 1).gameObject.SetActive(true);
-        objects.transform.GetChild(Json.Instance.data.nowGameStage - 1).gameObject.SetActive(true);
 
         key.transform.position = keyPos.transform.GetChild(Json.Instance.data.nowGameStage - 1).transform.position;
+        player.transform.position = playerPos.transform.GetChild(Json.Instance.data.nowGameStage - 1).transform.position;
 
         Json.Instance.Save();
     }
