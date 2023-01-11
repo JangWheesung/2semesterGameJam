2acf0329aa72db65a29d7e6f73f00b38e039aca7
nebula6684 <nebula6684@gmail.com>
2023-01-11 20:09:04 +0900

    totutuclear

--- Assets/Script/Manager/ClearManager.cs
+++ Assets/Script/Manager/ClearManager.cs
@@ -7,18 +7,26 @@ using FD.Dev;
 public class ClearManager : MonoBehaviour
 {
     [SerializeField] private GameObject nextButton;
+    [SerializeField] private RectTransform menuButton;
     [SerializeField] private GameObject endingText;
 
     void Start()
     {
         Json.Instance.Read();
-        if (Json.Instance.data.maxGameStage == 0) nextButton.SetActive(false); Json.Instance.data.maxGameStage = 1;
-        if (Json.Instance.data.maxGameStage > 12) nextButton.SetActive(false); FAED.InvokeDelay(() => { endingText.transform.DOMoveY(0, 1f);}, 0.3f);
+        if (Json.Instance.data.maxGameStage == 0)
+        {
+            nextButton.SetActive(false);
+            menuButton.position = new Vector3(0, menuButton.position.y, 0);
+            menuButton.position += new Vector3(0, 0, 648);
+            Json.Instance.data.maxGameStage = 1;
+        }
+        if (Json.Instance.data.maxGameStage > 12)
+        {
+            nextButton.SetActive(false);
+            menuButton.position = new Vector3(0, menuButton.position.y, 0);
+            menuButton.position += new Vector3(0, 0, 648);
+            FAED.InvokeDelay(() => { endingText.transform.DOMoveY(0, 1f); }, 0.3f);
+        }
         Json.Instance.Save();
-    }
-
-    void Update()
-    {
-        
     }
 }
